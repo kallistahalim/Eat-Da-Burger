39f5e062f46cbcde1ae88bf37c9cd26cf99e4d75
kallistahalim <kallistahalim@gmail.com>
2018-12-28 10:33:46 -0800

    added schema.sql burger_db database

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+USE DATABASE burger_db;
+
+CREATE TABLE burgers (
+    ID INT AUTO_INCREMENT NOT NULL,
+    Burger_Name VARCHAR(255) NOT NULL,
+    Devoured BOOLEAN NOT NULL,
+    primary key (ID)
+);
+
