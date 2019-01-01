207d79034842be36f34c666bd42f9180c6a0c05b
kallistahalim <kallistahalim@gmail.com>
2019-01-01 11:28:02 -0800

    frontend pretty

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO burgers (Burger_Name, Devoured) VALUES ("Double Quater Pound King", TRUE);
+INSERT INTO burgers (Burger_Name, Devoured) VALUES ("Double Quarter Pound King", TRUE);
 INSERT INTO burgers (Burger_Name, Devoured) VALUES ("Whopper", FALSE);
